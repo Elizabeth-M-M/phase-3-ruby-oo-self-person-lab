e0ac0f68fe74853ebe14b699258b4b72adb6f9c7
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-11 14:18:24 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@
 class Person
   attr_reader :name
   attr_accessor :bank_account, :happiness, :hygiene
-
+# initialize
   def initialize(name)
     @name = name
     @bank_account=25
@@ -10,9 +10,8 @@ class Person
     @hygiene=8
 
   end
-
+# happiness setter
   def happiness=(happiness_level)
-    # @happiness= happiness_level
       if happiness_level<0
         @happiness= 0
       elsif happiness_level>10
@@ -21,10 +20,8 @@ class Person
         puts happiness_level
         @happiness= happiness_level
       end
-
   end
-
-
+# hygiene setter
   def hygiene=(hygiene_amount)
     if hygiene_amount<0
         @hygiene= 0
@@ -35,83 +32,57 @@ class Person
         @hygiene= hygiene_amount
       end
   end
-
+# clean method
   def clean?
-
     @hygiene>7
   end
+  # happy method
   def happy?
     @happiness>7
-
   end
+  # get paid method
   def get_paid(amount)
     @bank_account+=amount
     "all about the benjamins"
-
   end
+  # take bath method
   def take_bath
-
    self.hygiene=(@hygiene +4)
   "♪ Rub-a-dub just relaxing in the tub ♫"
-
   end
+  # work out method
   def work_out
    self.happiness=(@happiness+2)
     self.hygiene=(@hygiene-3)
-
     "♪ another one bites the dust ♫"
-
   end
-  def call_friend(friend) 
+  # call friend method
+  def call_friend(friend)
     puts friend.happiness
     self.happiness=(@happiness+3)
-    friend.happiness=(@happiness+3)
+    friend.happiness=(friend.happiness+3)
     "Hi #{friend.name}! It's #{@name}. How are you?"
   end
-
+# start conversation method
   def start_conversation(friend, topic)
-    people =[self, friend]
     if topic =="politics"
-      # self.happiness=(happiness-2)
-      # friend.happiness=(happiness-2)
-      people.each {|person| person.happiness=(happiness-2)}
+      self.happiness=(@happiness-2)
+      friend.happiness=(friend.happiness-2)
       "blah blah partisan blah lobbyist"
     elsif topic =="weather"
-        # self.happiness=(happiness+1)
-        # friend.happiness=(happiness+1)
-        people.each {|person| person.happiness=(happiness+1)}
+        self.happiness=(@happiness+1)
+        friend.happiness=(friend.happiness+1)
         "blah blah sun blah rain"
     else
-
         "blah blah blah blah blah"
     end
-
   end
-
 end
 
 p1= Person.new("Stella")
-
 p2= Person.new("Felix")
 
-# p1.happiness= 0
+puts p2.happiness
+puts p1.start_conversation(p2, "politics")
 
-#  puts p1.take_bath
-  # p1.bank_account = 39
-
-  puts p2.happiness
-  puts p1.call_friend(p2)
-  # puts p2.happiness
-  # puts p1.happiness
-  # puts p1.call_friend("felix")
-  # puts p1.happiness
-  # puts p1.hygiene
-  # puts p1.take_bath
-  # puts p1.hygiene
-  # puts p1.take_bath
-  # puts p1.happy?
-  # puts p1.get_paid(200)
-  # puts p1.bank_account
-  # puts p1.get_paid(200)
-  # puts p1.bank_account
 
